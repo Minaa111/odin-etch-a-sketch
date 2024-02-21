c94ba4891f4326240fa3e7b9249dfc38aad806fa
Mina <minaschool11@gmail.com>
2024-02-21 13:46:04 +0200

    Added some styles

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const gridContainer = document.querySelector('.grid-container');
 // Setting a default grid size
 size = 16;
 
+
+
 for(let i = 0; i < size; i++){
     // insert size numbered rows
     const row = document.createElement('div');
@@ -12,8 +14,14 @@ for(let i = 0; i < size; i++){
     for(let j = 0; j < size; j++){
         // insert size numbered squares into each row
         const square = document.createElement('div');
-        square.setAttribute('style', 'display: flex; flex-grow: 1;height: auto; width: auto; ');
+        square.setAttribute('style', 'display: flex; flex-grow: 1;height: auto; width: auto;');
         row.append(square)
+        square.addEventListener('mouseover', () => {
+            square.classList.add('hover-effect')
+        })
+        square.addEventListener('mouseout', () => {
+            square.classList.remove('hover-effect'); // Revert to default background color
+        });
     }
     
 }
